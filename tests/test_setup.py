801af343832711f01fcbc234bdc47014158fb998
Ethan Moss <ethan.moss@ons.gov.uk>
2023-06-16 15:00:40 +0100

    test: added simple install check; tweaked pre-commit depricated format check

---
+++ tests/test_setup.py
@@ -0,0 +1,30 @@
+"""test_setup.py.
+
+Unit tests for testing initial setup. The intention is these tests won't be
+part of the main test suite, and will only by run as needed.
+
+TODO: make this run 'on request' only.
+"""
+
+import pytest
+
+
+class TestSetup:
+    """A class of tests to check correct set-up.
+
+    A set of tests to:
+        1. check `heimdall_transport` is installed.
+    """
+
+    def test_heimdall_transport_install(self) -> None:
+        """Check `heimdall_transport is installed.
+
+        This is a simple test to check the package has been installed and is
+        available for use.
+        """
+        try:
+            import heimdall_transport as ht
+
+            assert ht.__name__ == "heimdall_transport"
+        except ImportError:
+            pytest.fail("Unable to find `heimdall_transport`.")
