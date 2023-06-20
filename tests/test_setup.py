4595204e137516632a3342d15865e11a5d26673a
r-leyshon <richard.leyshon@ons.gov.uk>
2023-06-20 14:37:04 +0100

    test: Check setup on java install

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -7,6 +7,8 @@ TODO: make this run 'on request' only.
 """
 
 import pytest
+import os
+from r5py import TransportNetwork
 
 
 class TestSetup:
@@ -29,3 +31,37 @@ class TestSetup:
             assert ht.__name__ == "heimdall_transport"
         except ImportError:
             pytest.fail("Unable to find `heimdall_transport`.")
+
+    @pytest.mark.setup
+    def test_r5py_setup(self) -> None:
+        """Check development environment will cope with r5py requirements."""
+        # search the ext dir for pbf & gtfs
+        search_pth = os.path.join("tests", "data")
+        foundf = os.listdir(search_pth)
+        gtfs = [
+            os.path.join(search_pth, x) for x in foundf if x.endswith(".zip")
+        ][0]
+        pbf = [
+            os.path.join(search_pth, x) for x in foundf if x.endswith(".pbf")
+        ][0]
+
+        # needs wrapping in try but specific exception to raise unknown.
+        # Examining r5py exception classes, I'll go with the below.
+        try:
+            TransportNetwork(pbf, [gtfs])
+        except RuntimeError:
+            print("RuntimeError encountered")
+            pass
+        except MemoryError:
+            print("Memory error encountered")
+            pass
+
+        # has a .mapdb file been created in the external directory?
+        mapdb_f = pbf + ".mapdb"
+        mapdb_p_f = pbf + ".mapdb.p"
+        assert os.path.exists(
+            mapdb_f
+        ), f"r5py did not create the expected file at:/ {mapdb_f}"
+        assert os.path.exists(
+            mapdb_p_f
+        ), f"r5py did not create the expected file at:/ {mapdb_p_f}"
