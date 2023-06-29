a63a51ce136c6cff13cc6c809e5e2b60faa47d1a
r-leyshon <richard.leyshon@ons.gov.uk>
2023-06-29 12:11:50 +0100

    refactor: Use class to handle mocked return values

--- tests/gtfs/test_routes.py
+++ tests/gtfs/test_routes.py
@@ -5,6 +5,20 @@ import pandas as pd
 from heimdall_transport.gtfs.routes import scrape_route_type_lookup
 
 
+def mocked__get_response_text(*args, **kwargs):
+    """Mock _get_response_text.
+
+    Returns
+    -------
+        str: Minimal text representation of url tables.
+
+    """
+    key1 = "https://gtfs.org/schedule/reference/"
+    val1 = "<td> <br><code>0</code> - Tram.<br><code>1</code>"
+    return_vals = {key1: val1}
+    return return_vals[args[0]]
+
+
 class TestScrapeRouteTypeLookup(object):
     """Test scrape_route_type_lookup."""
 
@@ -35,7 +49,7 @@ class TestScrapeRouteTypeLookup(object):
         """Check the return object when extended_schema = False."""
         mocker.patch(
             "heimdall_transport.gtfs.routes._get_response_text",
-            return_value="<td> <br><code>0</code> - Tram.<br><code>1</code>",
+            side_effect=mocked__get_response_text,
         )
         result = scrape_route_type_lookup(extended_schema=False)
         assert isinstance(result, pd.core.frame.DataFrame)
