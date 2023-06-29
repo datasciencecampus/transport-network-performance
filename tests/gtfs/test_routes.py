fbf32d832e763769ef5aa8a22af982760138e088
r-leyshon <richard.leyshon@ons.gov.uk>
2023-06-29 11:30:58 +0100

    test: Check expected lookup is returned

--- tests/gtfs/test_routes.py
+++ tests/gtfs/test_routes.py
@@ -1,5 +1,6 @@
 """Testing routes module."""
 import pytest
+import pandas as pd
 
 from heimdall_transport.gtfs.routes import scrape_route_type_lookup
 
@@ -29,3 +30,16 @@ class TestScrapeRouteTypeLookup(object):
             match=r"`extended_schema` expected boolean. Got <class 'str'>",
         ):
             scrape_route_type_lookup(extended_schema="True")
+
+    def test_table_without_extended_schema(self, mocker):
+        """Check the return object when extended_schema = False."""
+        mocker.patch(
+            "heimdall_transport.gtfs.routes._get_response_text",
+            return_value="<td> <br><code>0</code> - Tram.<br><code>1</code>",
+        )
+        result = scrape_route_type_lookup(extended_schema=False)
+        assert isinstance(result, pd.core.frame.DataFrame)
+        pd.testing.assert_frame_equal(
+            result,
+            pd.DataFrame({"route_type": "0", "desc": "Tram."}, index=[0]),
+        )
