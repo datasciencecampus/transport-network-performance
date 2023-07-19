4c9a3647785daf46e4ef986e89195e1fe2780f09
Ethan Moss <ethan.moss@ons.gov.uk>
2023-07-19 14:05:22 +0100

    feat: raster merge input file handling

--- src/transport_performance/utils/raster.py
+++ src/transport_performance/utils/raster.py
@@ -6,9 +6,18 @@ to 200x200m grids). The original design intention is for these to form part of
 gridded population data pre-processing.
 """
 
+import os
+import glob
+import re
+
+from pyprojroot import here
+
 
 def merge_raster_files(
-    input_dir: str, output_dir: str, output_filename: str
+    input_dir: str,
+    output_dir: str,
+    output_filename: str,
+    subset_regex: str = None,
 ) -> None:
     """Merge raster files together.
 
@@ -18,14 +27,29 @@ def merge_raster_files(
     Parameters
     ----------
     input_dir : str
-        Directory containing input raster files
+        Directory containing input raster files.
     output_dir : str
-        Directory to write output, merged raster file
+        Directory to write output, merged raster file.
     output_filename : str
-        Filename of merged raster file (.tif extension required)
+        Filename of merged raster file (.tif extension required).
+    subset_regex : str, optional
+        Subset any raster files in the input directory using a regex, by
+        default None which means no subsetting will occur.
 
     """
-    pass
+    # defend against case where the provided input dir does not exist
+    if not os.path.exists(input_dir):
+        raise FileNotFoundError(f"{input_dir} can not be found")
+
+    # get tif files in directory, ensure some exist and select subset via regex
+    tif_filepaths = glob.glob(f"{input_dir}/*.tif")
+    if len(tif_filepaths) == 0:
+        raise FileNotFoundError(f"No `*.tif` files found in {input_dir}")
+    if subset_regex is not None:
+        tif_filepaths = [
+            fpath for fpath in tif_filepaths if re.search(subset_regex, fpath)
+        ]
+    print(len(tif_filepaths))
 
 
 def sum_resample_file(
@@ -57,3 +81,12 @@ def sum_resample_file(
 
     """
     pass
+
+
+# TODO: remove once development is completed
+if __name__ == "__main__":
+
+    # set inputs, checking with E020 in filename
+    INPUT_FOLDER = os.path.join(here(), "data", "external", "population")
+    subset_regex = r"(.{0,})(E2020)(.{0,})(\.tif)$"
+    merge_raster_files(INPUT_FOLDER, "", "", subset_regex=subset_regex)
