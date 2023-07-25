f054283257bba1dbc6fbb89888e8a8735a452524
r-leyshon <richard.leyshon@ons.gov.uk>
2023-07-25 15:03:02 +0100

    test: Defensive check pbf_pth

--- tests/osm/test_osm_utils.py
+++ tests/osm/test_osm_utils.py
@@ -1,4 +1,8 @@
 """Test osm_utils module."""
+import pytest
+from pyprojroot import here
+
+from transport_performance.osm.osm_utils import filter_osm
 
 
 class TestFilterOsm(object):
@@ -6,4 +10,14 @@ class TestFilterOsm(object):
 
     def test_filter_osm_defense(self):
         """Defensive behaviour for filter_osm."""
-        return None
+        with pytest.raises(
+            FileExistsError, match="not/a/pbf/.nosiree not found on file."
+        ):
+            # file doesnt exist
+            filter_osm(pbf_pth="not/a/pbf/.nosiree")
+        with pytest.raises(
+            ValueError,
+            match="`pbf_pth` expected file extension .pbf. Found .zip",
+        ):
+            # file exists but is not a pbf
+            filter_osm(pbf_pth=here("tests/data/newport-20230613_gtfs.zip"))
