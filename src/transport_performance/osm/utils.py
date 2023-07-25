4b93c34f46acea30959616361032c7ae50065a9d
r-leyshon <richard.leyshon@ons.gov.uk>
2023-07-25 12:26:37 +0100

    feat: Optionally tries to install osmosis

--- src/transport_performance/osm/utils.py
+++ src/transport_performance/osm/utils.py
@@ -14,8 +14,9 @@ def filter_osm(
     out_pth="filtered.osm.pbf",
     bbox=[-3.01, 51.58, -2.99, 51.59],
     tag_filter=True,
+    install_osmosis=False,
 ):
-    """Filter an osm.pbf file to a bounding box. Relies on homebrew.
+    """Filter an osm.pbf file to a bbox. Relies on homebrew with osmosis.
 
     Parameters
     ----------
@@ -30,13 +31,13 @@ def filter_osm(
     tag_filter: (bool, optional)
         Should non-highway ways be filtered? Excludes waterway, landuse &
         natural. Defaults to True.
+    install_osmosis: (bool, optional)
+        Should brew be used to install osmosis if not found. Defaults to False.
 
     Raises
     ------
-        NameError: Brew has no formula 'osmosis'.
         Exception: Subprocess error.
-        Exception: FileNotFoundError - osmosis not compatible with executed
-        command.
+        Exception: Osmosis not found. Will not raise if install_osmosis=True.
 
     Returns
     -------
@@ -47,24 +48,11 @@ def filter_osm(
     _bool_defence(tag_filter, param_nm="tag_filter")
     _check_list(bbox, param_nm="bbox", check_elements=True, exp_type=float)
     _check_parent_dir_exists(out_pth, param_nm="out_pth", create=True)
-    # check osmosis is installed
-    check_cmd = ["brew", "deps", "--tree", "--installed", "osmosis"]
-    try:
-        subprocess.run(
-            check_cmd,
-            check=True,
-        )
-    except subprocess.CalledProcessError as e1:
-        raise NameError(f"No formula with name 'osmosis': {e1}")
-
-    print(type(pbf_pth))
-    inpth = pbf_pth.as_posix()
-
     # Compile the osmosis command
     cmd = [
         "osmosis",
         "--read-pbf",
-        inpth,
+        pbf_pth.as_posix(),
         "--bounding-box",
         f"left={bbox[0]}",
         f"bottom={bbox[1]}",
@@ -83,6 +71,16 @@ def filter_osm(
     except subprocess.CalledProcessError as e2:
         raise Exception(f"Error executing osmosis command: {e2}")
     except FileNotFoundError as e3:
-        raise Exception(f"The osmosis command was not recognised: {e3}")
+        if install_osmosis:
+            print(f"osmosis command was not recognised: {e3}. Trying install.")
+            subprocess.run(["brew", "install", "osmosis"])
+            print("Installation of `osmosis successful.`")
+            subprocess.run(cmd, check=True)
+            print(f"Retry filter pbf completed. Written to {out_pth}")
+        else:
+            raise Exception("`osmosis` is not found. Please install.")
 
     return None
+
+
+filter_osm(install_osmosis=True)
