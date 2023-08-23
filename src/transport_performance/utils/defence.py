5149dc28ecd510fb85f571e8419c3b856bc0eeda
Browning <Charlie.Browning@ons.gov.uk>
2023-08-23 13:53:38 +0100

    Updated _check_parent_directory_exists() to account for different path formats

--- src/transport_performance/utils/defence.py
+++ src/transport_performance/utils/defence.py
@@ -30,10 +30,13 @@ def _is_path_like(pth, param_nm):
 
 def _check_parent_dir_exists(pth, param_nm, create=False):
     _is_path_like(pth, param_nm)
-    # realpath helps to catch cases where relative paths are passed in main
-    pth = os.path.realpath(pth)
+    # replace back slashes to ensure consistency
+    pth = pth.replace("\\", "/").replace(repr("\\"), "").replace("'", "")
+    pth = pth.replace("\\", "/")
     # convert path to the correct OS specific format
     pth = pathlib.Path(pth)
+    # realpath helps to catch cases where relative paths are passed in main
+    pth = os.path.realpath(pth)
     parent = os.path.dirname(pth)
     if not os.path.exists(parent):
         if create:
