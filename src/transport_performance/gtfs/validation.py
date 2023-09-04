64fdbd977aafb11a4c30ed588509e697c619e984
Browning <Charlie.Browning@ons.gov.uk>
2023-09-04 10:42:25 +0100

    Add skelentons for fast travel checks

--- src/transport_performance/gtfs/validation.py
+++ src/transport_performance/gtfs/validation.py
@@ -601,3 +601,16 @@ class GtfsInstance:
         )
         self.route_mode_summary_df = out_tab
         return self.route_mode_summary_df
+
+    def validate_travel_between_consecutive_stops(self):
+        """Validate the travel between consecutive stops in the GTFS data.
+
+        Ensures that a trip is valid by examining the duration and distance
+        of a trip. If a vehicle is travelling at an unusual speed, the trip can
+        be deemed invalid.
+        """
+        pass
+
+    def validate_travel_over_multiple_stops(self):
+        """Validate travel over multiple stops in the GTFS data."""
+        pass
