02f9c6aae5fabb71fe823bf1304894ae4358fa35
Browning <Charlie.Browning@ons.gov.uk>
2023-09-08 12:09:20 +0100

    Update drop_trip()->drop_trips()

--- src/transport_performance/gtfs/cleaners.py
+++ src/transport_performance/gtfs/cleaners.py
@@ -1,28 +1,45 @@
 """A set of functions that clean the gtfs data."""
+from typing import Union
+
+import numpy as np
+
+from transport_performance.utils.defence import _gtfs_defence
 
 
-def drop_trip(gtfs, trip_id: str) -> None:
-    """Drop a trip from a GtfsInstance object.
+def drop_trips(gtfs, trip_id: Union[str, list, np.ndarray]) -> None:
+    """Drop trip{s} from a GtfsInstance object.
 
     Parameters
     ----------
     gtfs : GtfsInstance
-        The GtfsInstance object to drop the trip from
-    trip_id : str
-        The trip ID of the trip to be dropped from the gtfs data.
+        The GtfsInstance object to drop the trip(s) from
+    trip_id : Union[str, list, np.ndarray]
+        The trip ID(s) of the trip to be dropped from the gtfs data.
 
     Returns
     -------
     None
 
     """
+    # defences
+    _gtfs_defence(gtfs, "gtfs")
+    if not isinstance(trip_id, (str, list, np.ndarray)):
+        raise TypeError(
+            f"'trip_id recieved type: {type(trip_id)}."
+            "Expected types: [str, list, np.ndarray]"
+        )
+
+    # ensure trip ID is an iterable
+    if isinstance(trip_id, str):
+        trip_id = [trip_id]
+
     # obtain all releveant IDs
-    trip_info = gtfs.feed.trips[gtfs.feed.trips["trip_id"] == trip_id]
-    route_id = list(trip_info["route_id"])[0]
-    service_id = list(trip_info["service_id"])[0]
-    shape_id = list(trip_info["shape_id"])[0]
+    trip_info = gtfs.feed.trips[gtfs.feed.trips["trip_id"].isin(trip_id)]
+    route_id = list(trip_info["route_id"])
+    service_id = list(trip_info["service_id"])
+    shape_id = list(trip_info["shape_id"])
     stop_ids = gtfs.feed.stop_times[
-        gtfs.feed.stop_times["trip_id"] == trip_id
+        gtfs.feed.stop_times["trip_id"].isin(trip_id)
     ]["stop_id"].unique()
 
     # drop relevant records from tables
@@ -31,18 +48,26 @@ def drop_trip(gtfs, trip_id: str) -> None:
         ~gtfs.feed.stops["stop_id"].isin(stop_ids)
     ]
     gtfs.feed.calendar = gtfs.feed.calendar[
-        gtfs.feed.calendar["service_id"] != service_id
+        ~gtfs.feed.calendar["service_id"].isin(service_id)
     ]
     gtfs.feed.stop_times = gtfs.feed.stop_times[
-        gtfs.feed.stop_times["trip_id"] != trip_id
+        ~gtfs.feed.stop_times["trip_id"].isin(trip_id)
     ]
     gtfs.feed.routes = gtfs.feed.routes[
-        gtfs.feed.routes["route_id"] != route_id
+        ~gtfs.feed.routes["route_id"].isin(route_id)
     ]
     gtfs.feed.shapes = gtfs.feed.shapes[
-        gtfs.feed.shapes["shape_id"] != shape_id
+        ~gtfs.feed.shapes["shape_id"].isin(shape_id)
     ]
 
     # re-run so that summaries can be updated
     gtfs.pre_processed_trips = gtfs._preprocess_trips_and_routes()
     return None
+
+
+# def clean_consecutive_stop_fast_travel_warnings(gtfs):
+#     pass
+
+
+# def clean_multiple_stop_fast_travel_warnings(gtfs):
+#     pass
