61b527370a93a52978d725692abe24d6d8a74f71
Browning <Charlie.Browning@ons.gov.uk>
2023-10-10 14:30:08 +0100

    Replace _is_expected_filetype() with file extensions enforcing

--- src/transport_performance/gtfs/gtfs_utils.py
+++ src/transport_performance/gtfs/gtfs_utils.py
@@ -13,6 +13,7 @@ from transport_performance.utils.defence import (
     _is_expected_filetype,
     _check_list,
     _type_defence,
+    _enforce_file_extension,
 )
 
 
@@ -81,9 +82,7 @@ def bbox_filter_gtfs(
 
     # check paths have valid zip extensions
     _is_expected_filetype(pth=in_pth, param_nm="in_pth")
-    _is_expected_filetype(
-        pth=out_pth, param_nm="out_pth", check_existing=False
-    )
+    _enforce_file_extension(out_pth, ".zip", ".zip", "out_pth")
 
     if isinstance(bbox, list):
         _check_list(ls=bbox, param_nm="bbox_list", exp_type=float)
