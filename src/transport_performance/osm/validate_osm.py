fbd154e0bc3ae7de5ad028cb3393c70437bfce6d
r-leyshon <richard.leyshon@ons.gov.uk>
2023-10-16 14:16:42 +0100

    refactor: Src contains no api usage

--- src/transport_performance/osm/validate_osm.py
+++ src/transport_performance/osm/validate_osm.py
@@ -2,7 +2,6 @@
 import osmium
 import warnings
 from typing import Any
-from pyprojroot import here
 from pathlib import Path
 from typing import Union
 
@@ -14,11 +13,20 @@ from transport_performance.utils.defence import (
 )
 
 
-PBF_FIX_PTH = here("tests/data/newport-2023-06-13.osm.pbf")
-WALES_LATEST_PTH = here("data/external/osm/wales-latest.osm.pbf")
-
-
 def _compile_tags(osmium_feature):
+    """Return tag name value pairs.
+
+    Parameters
+    ----------
+    osmium_feature : osmium.osm.types
+        Either a node, way, relation or area osmium type.
+
+    Returns
+    -------
+    tagdict: dict
+        A dictionary of tag name & values.
+
+    """
     tagdict = dict()
     for tag in osmium_feature.tags:
         # taglist.append(tag)
@@ -150,28 +158,6 @@ class GenericHandler(osmium.SimpleHandler):
         self.area_tags[a.id] = tagdict
 
 
-i = GenericHandler()
-i.apply_file(PBF_FIX_PTH, locations=False)
-
-# counts of features.
-len(i.node_ids)
-len(i.way_ids)
-len(i.relations_ids)
-len(i.area_ids)
-
-i.node_locs[10971292662]
-i.node_tags[10971292664]
-
-i.relations_tags[15775326]
-i.relations_members[15775326]  # note that internally, osmium will prepend the
-# tag with the type of feature, eg r15775326 is a relation, n15775326 would be
-# a node etc.
-i.way_nodes[1181392035]
-i.way_tags[1181392039]
-
-i.area_tags[16417043]
-
-
 class _IdHandler(osmium.SimpleHandler):
     """Collate Ids for OSM Features.
 
@@ -236,7 +222,7 @@ class _IdHandler(osmium.SimpleHandler):
         self.area_ids.append(a.id)
 
 
-class TagHandler(osmium.SimpleHandler):
+class _TagHandler(osmium.SimpleHandler):
     """Collate tags and check them for specified IDs.
 
     Parameters
@@ -305,58 +291,6 @@ class TagHandler(osmium.SimpleHandler):
         tagdict = _compile_tags(a)
         self.area_tags[a.id] = tagdict
 
-    def check_tags_for_ids(self, ids: list, feature_type: str) -> dict:
-        """Return tags for provided list of feature IDs.
-
-        Parameters
-        ----------
-        ids : list
-            A list of OSM feature IDs to check. IDs must be integer.
-        feature_type : str
-            The type of feature to which the IDS belong. Valid options are
-            ["node", "way", "relation", "area"].
-
-        Returns
-        -------
-        dict
-            ID: dict of tags, containing tag name : value.
-
-        Raises
-        ------
-        ValueError
-            `feature_type` is not one of "node", "way", "relation" or "area".
-        TypeError
-            `ids` is not a list.
-            Elements of `ids` are not integer.
-            `feature_type` is not a string.
-
-        """
-        # defence
-        _check_list(ids, "ids", exp_type=int)
-        _type_defence(feature_type, "feature_type", str)
-        t = feature_type.lower().strip()
-        _check_item_in_list(
-            t, ["node", "way", "relation", "area"], "feature_type"
-        )
-        # return the tags for the appropriate feature_type
-        if t == "node":
-            target = self.node_tags
-        elif t == "way":
-            target = self.way_tags
-        elif t == "relation":
-            target = self.relation_tags
-        else:
-            target = self.area_tags
-
-        result = dict((id, target[id]) for id in ids if id in target)
-
-        if len(result) == 0:
-            raise ValueError(
-                "No tags found. Did you specify the correct feature_type?"
-            )
-
-        return result
-
 
 class FindIds(_IdHandler):
     """Apply ID collation to an OSM file.
@@ -393,7 +327,7 @@ class FindIds(_IdHandler):
         _is_expected_filetype(
             osm_pth, "osm_pth", check_existing=True, exp_ext=".pbf"
         )
-        self.apply_file(PBF_FIX_PTH)
+        self.apply_file(osm_pth)
         self.counts = dict()
         self.id_dict = dict()
 
@@ -447,22 +381,89 @@ class FindIds(_IdHandler):
         return id_dict
 
 
-ids = FindIds(osm_pth=PBF_FIX_PTH)
-ids.count_features()
-ids.get_feature_ids()
-ids.id_dict["way_ids"][0:11]
+class FindTags(_TagHandler):
+    """Applies tag collation to OSM file. Option to check tags for given IDs.
 
-tags = TagHandler()
-tags.apply_file(PBF_FIX_PTH)
-tags.node_tags[10971292662]  # subset by a single ID of interest
-# many features come without tags, this slice of available nodes shows how to
-# return tags for found feature IDs, some with empty and some with populated
-# tag lists.
-# Get a sliced list of IDs to investigate
-tags.check_tags_for_ids(ids.id_dict["node_ids"][23:38], "node")
-# get some way tags
-tags.check_tags_for_ids(ids.id_dict["way_ids"][0:11], "way")
-# get some relation tags
-tags.check_tags_for_ids(ids.id_dict["relation_ids"][0:11], "relation")
-# get some area tags
-tags.check_tags_for_ids(ids.id_dict["area_ids"][0:11], "area")
+    Parameters
+    ----------
+    _TagHandler : class
+        Internal class for handling IDs. Inherits from osmium.SimpleHandler.
+    osm_pth: Union[Path, str]
+        Path to osm file.
+
+    Raises
+    ------
+    TypeError:
+        `osm_pth` is not of type pathlib.Path or str.
+    FileNotFoundError:
+        `osm_pth` file not found on disk.
+    ValueError:
+        `osm_pth` does not have a .pbf extension.
+
+    Attributes
+    ----------
+    found_tags: dict
+        Found tags for specified feature IDs.
+
+    """
+
+    def __init__(self, osm_pth) -> None:
+        super().__init__()
+        _is_expected_filetype(
+            osm_pth, "osm_pth", check_existing=True, exp_ext=".pbf"
+        )
+        self.apply_file(osm_pth)
+        self.found_tags = dict()
+
+    def check_tags_for_ids(self, ids: list, feature_type: str) -> dict:
+        """Return tags for provided list of feature IDs.
+
+        Parameters
+        ----------
+        ids : list
+            A list of OSM feature IDs to check. IDs must be integer.
+        feature_type : str
+            The type of feature to which the IDS belong. Valid options are
+            ["node", "way", "relation", "area"].
+
+        Returns
+        -------
+        found_tags: dict
+            ID: dict of tags, containing tag name : value.
+
+        Raises
+        ------
+        ValueError
+            `feature_type` is not one of "node", "way", "relation" or "area".
+        TypeError
+            `ids` is not a list.
+            Elements of `ids` are not integer.
+            `feature_type` is not a string.
+
+        """
+        # defence
+        _check_list(ids, "ids", exp_type=int)
+        _type_defence(feature_type, "feature_type", str)
+        t = feature_type.lower().strip()
+        _check_item_in_list(
+            t, ["node", "way", "relation", "area"], "feature_type"
+        )
+        # return the tags for the appropriate feature_type
+        if t == "node":
+            target = self.node_tags
+        elif t == "way":
+            target = self.way_tags
+        elif t == "relation":
+            target = self.relation_tags
+        else:
+            target = self.area_tags
+
+        found_tags = dict((id, target[id]) for id in ids if id in target)
+
+        if len(found_tags) == 0:
+            raise ValueError(
+                "No tags found. Did you specify the correct feature_type?"
+            )
+        self.found_tags = found_tags
+
+        return found_tags
