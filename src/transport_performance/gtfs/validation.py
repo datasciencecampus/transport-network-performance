5f70a13c1da0ec328c295eb3fce64b8339fc5308
Browning <Charlie.Browning@ons.gov.uk>
2023-10-20 11:19:59 +0100

    move constants from the gtfs module

--- src/transport_performance/gtfs/validation.py
+++ src/transport_performance/gtfs/validation.py
@@ -42,7 +42,10 @@ from transport_performance.gtfs.report.report_utils import (
     TemplateHTML,
     _set_up_report_dir,
 )
-from transport_performance.utils.constants import PKG_PATH
+from transport_performance.utils.constants import (
+    PKG_PATH,
+    GTFS_UNNEEDED_COLUMNS,
+)
 
 
 def _get_intermediate_dates(
@@ -290,39 +293,6 @@ class GtfsInstance:
             self.ROUTE_LKP = get_saved_route_type_lookup(path=route_lookup_pth)
         else:
             self.ROUTE_LKP = get_saved_route_type_lookup()
-        # Constant to remove non needed columns from repeated
-        # pair error information.
-        # This is a messy method however it is the only
-        # way to ensure that the error report remains
-        # dynamic and can adadpt to different tables
-        # in the GTFS file.
-
-        self.GTFS_UNNEEDED_COLUMNS = {
-            "routes": [],
-            "agency": ["agency_phone", "agency_lang"],
-            "stop_times": [
-                "stop_headsign",
-                "pickup_type",
-                "drop_off_type",
-                "shape_dist_traveled",
-                "timepoint",
-            ],
-            "stops": [
-                "wheelchair_boarding",
-                "location_type",
-                "parent_station",
-                "platform_code",
-            ],
-            "calendar_dates": [],
-            "calendar": [],
-            "trips": [
-                "trip_headsign",
-                "block_id",
-                "shape_id",
-                "wheelchair_accessible",
-            ],
-            "shapes": [],
-        }
 
     def get_gtfs_files(self) -> list:
         """Return a list of files making up the GTFS file.
@@ -1324,7 +1294,7 @@ class GtfsInstance:
                 )
                 drop_cols = [
                     col
-                    for col in self.GTFS_UNNEEDED_COLUMNS[table]
+                    for col in GTFS_UNNEEDED_COLUMNS[table]
                     if col not in join_vars
                 ]
                 filtered_tbl = table_map[table].copy().drop(drop_cols, axis=1)
--- src/transport_performance/utils/constants.py
+++ src/transport_performance/utils/constants.py
@@ -3,3 +3,53 @@ import transport_performance
 from importlib import resources as pkg_resources
 
 PKG_PATH = pkg_resources.files(transport_performance)
+
+# GTFS
+
+# Constant to remove non needed columns from repeated
+# pair error information.
+# This is a messy method however it is the only
+# way to ensure that the error report remains
+# dynamic and can adadpt to different tables
+# in the GTFS file.
+GTFS_UNNEEDED_COLUMNS = {
+    "routes": [],
+    "agency": ["agency_phone", "agency_lang"],
+    "stop_times": [
+        "stop_headsign",
+        "pickup_type",
+        "drop_off_type",
+        "shape_dist_traveled",
+        "timepoint",
+    ],
+    "stops": [
+        "wheelchair_boarding",
+        "location_type",
+        "parent_station",
+        "platform_code",
+    ],
+    "calendar_dates": [],
+    "calendar": [],
+    "trips": [
+        "trip_headsign",
+        "block_id",
+        "shape_id",
+        "wheelchair_accessible",
+    ],
+    "shapes": [],
+}
+
+# a constant containing the max acceptable speed of a route type (vehicle type)
+GTFS_VEHICLE_SPEED_BOUNDS = {
+    0: 100,
+    1: 150,
+    2: 500,
+    3: 150,
+    4: 80,
+    5: 30,
+    6: 50,
+    7: 50,
+    11: 150,
+    12: 150,
+    200: 120,
+}
