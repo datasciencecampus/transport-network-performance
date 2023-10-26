44afda8d5be36bfbd260b74fa5e2ee4c1b192f3a
Sergio Recio <sergio.recio.rodriguez@ons.gov.uk>
2023-10-26 10:56:50 +0100

    docs: improved docstrings

--- src/transport_performance/analyse_network/analyse_network.py
+++ src/transport_performance/analyse_network/analyse_network.py
@@ -70,6 +70,11 @@ class AnalyseNetwork:
         -------
         None
 
+        Notes
+        -----
+        This function will work with any number of origins between 1 and all.
+        However, this is not currently optimised and performance will be poor.
+
         """
         for sel_orig, sel_dest in tqdm(
             self._gdf_batch_origins(self.gdf, num_origins=num_origins),
@@ -176,6 +181,16 @@ class AnalyseNetwork:
         sel_dest : list
             Id numbers of the destinations in a given iteration.
 
+        Notes
+        -----
+        This function will filter out pairs of origins and destinations that
+        are over the distance threshold, and it will return a list with
+        unique origins and filtered destinations. However, note that this will
+        only work as intended if using a single origin. If using several
+        origins per batch, the r5py `TravelTimeMatrixComputer` object will
+        still create all possible combinations of origins and destinations
+        provided, which may include pairs that are beyond the threshold.
+
         """
         # TODO: add defences.
 
@@ -191,30 +206,33 @@ class AnalyseNetwork:
         # TODO: REMEMBER TO REMOVE SHUFFLE
         np.random.shuffle(origins)
 
-        # shuffles list of origins and loops through origins using selected
-        # amount of origins
+        # loops through origins using selected amount of origins
         for o in range(0, len(origins), num_origins):
             sel_origins = origins[o : o + num_origins]
 
+            # calculates cross join of origins and destinations
             full_gdf = orig_gdf[orig_gdf["id"].isin(sel_origins)].merge(
                 dest_gdf, how="cross", suffixes=["_orig", "_dest"]
             )
 
+            # calculates haversine distance between origins and destinations
             full_gdf["distance"] = self._haversine_gdf(
                 full_gdf, "geometry_orig", "geometry_dest"
             )
 
+            # filters out pairs where distance is over threshold
             sel_dest = list(
                 full_gdf[full_gdf["distance"] < distance]["id_dest"].unique()
             )
 
+            # yields lists with selected origins and destinations
             yield sel_origins, sel_dest
 
     def _haversine_gdf(
         self,
         gdf: gpd.GeoDataFrame,
-        col1: str,
-        col2: str,
+        orig: str,
+        dest: str,
         unit: Unit = Unit.KILOMETERS,
     ) -> np.array:
         """Calculate haversine distance between shapely Point objects.
@@ -224,9 +242,9 @@ class AnalyseNetwork:
         gdf : gpd.GeoDataFrame
             Cartesian product of origin and destinations geodataframes. Should
             contain a geometry for each.
-        col1 : str
+        orig : str
             Name of the column containing the origin geometry.
-        col2 : str
+        dest : str
             Name of the column containing the destination geometry.
         unit : Unit
             Unit to calculte distance.
@@ -239,8 +257,8 @@ class AnalyseNetwork:
         """
         # TODO: add defences.
 
-        lat_long_orig = gdf[col1].apply(lambda x: (x.y, x.x))
-        lat_long_dest = gdf[col2].apply(lambda x: (x.y, x.x))
+        lat_long_orig = gdf[orig].apply(lambda x: (x.y, x.x))
+        lat_long_dest = gdf[dest].apply(lambda x: (x.y, x.x))
 
         dist_array = haversine_vector(
             list(lat_long_orig), list(lat_long_dest), unit=unit
