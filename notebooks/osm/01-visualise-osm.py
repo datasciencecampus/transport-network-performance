398fd7c1f07f609888adc9372c19479a81d21a47
r-leyshon <richard.leyshon@ons.gov.uk>
2023-11-08 14:41:26 +0000

    refactor: Pipeline usage of the api

--- notebooks/osm/01-visualise-osm.py
+++ notebooks/osm/01-visualise-osm.py
@@ -2,7 +2,6 @@
 # %%
 from pyprojroot import here
 from transport_performance.osm import validate_osm as osmval
-from transport_performance.osm.validate_osm import _convert_osmdict_to_gdf
 
 # %%
 fix_pth = here("tests/data/newport-2023-06-13.osm.pbf")
@@ -11,22 +10,19 @@ fix_pth = here("tests/data/newport-2023-06-13.osm.pbf")
 # get some Node IDs to work with
 ids = osmval.FindIds(fix_pth)
 ids.get_feature_ids()
-node_IDs = ids.id_dict["node_ids"][0:1000]
-
-# %%
-# get the locations
-locs = osmval.FindLocations(fix_pth)
-node_coords = locs.check_locs_for_ids(node_IDs, "node")["node"]
-
-
-# %%
-nodes_gdf = _convert_osmdict_to_gdf(node_coords)
-nodes_gdf.explore()
-
-
-# %%
+node_ids = ids.id_dict["node_ids"][0:1000]
+# get a way ID to work with
 way_ids = ids.id_dict["way_ids"][0:1]
-way_coords = locs.check_locs_for_ids(way_ids, "way")["way"]
+
 # %%
-ways_gdf = _convert_osmdict_to_gdf(way_coords, "way")
-ways_gdf.explore()
+# find all location data in the fixture file
+locs = osmval.FindLocations(fix_pth)
+# %%
+# plot the subset of node IDs
+locs.plot_ids(ids=node_ids, feature_type="node")
+
+# %%
+# plot the selected way ID
+locs.plot_ids(ids=way_ids, feature_type="way")
+
+# %%
