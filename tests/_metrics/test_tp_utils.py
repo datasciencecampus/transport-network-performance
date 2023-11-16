307b1566fe44dc458e5bfab53c28ea26c0064cf7
Ethan Moss <ethan.moss@ons.gov.uk>
2023-11-16 18:29:51 +0000

    test: stats optional argument behaviour

--- tests/_metrics/test_tp_utils.py
+++ tests/_metrics/test_tp_utils.py
@@ -126,3 +126,58 @@ class TestTransportPerformanceStats:
                 urban_centre_gdf=uc_fixture.to_crs("EPSG:4326"),
             )
             assert_frame_equal(stats_df, expected_stats)
+
+    @pytest.mark.parametrize(
+        "arg_name, drop_expected_column",
+        [
+            ("urban_centre_name", "urban centre name"),
+            ("urban_centre_country", "urban centre country"),
+            ("urban_centre_gdf", "urban centre area"),
+        ],
+    )
+    def test__transport_performance_stats_optional_arguments(
+        self,
+        expected_transport_performance,
+        arg_name,
+        drop_expected_column,
+        uc_fixture,
+    ) -> None:
+        """Test `_transport_performance_stats()` optional argument behaviour.
+
+        Setting optional arguments to None in this function means that the
+        respective results do no appear in the descritive stats. This test
+        checks that behaviour
+
+        Parameters
+        ----------
+        expected_transport_performance
+            Mock transport performance input (using expected output), and
+            expected stats outputs.
+        arg_name
+            Optional argument to test.
+        drop_expected_column
+            Column to drop from expected results when the `arg_name` argument
+            is provided.
+        uc_fixture
+            A mock urban centre test fixture.
+
+        """
+        # store default argumnets
+        default_args = {
+            "urban_centre_name": "name",
+            "urban_centre_country": "country",
+            "urban_centre_gdf": uc_fixture,
+        }
+
+        # get mock tp data and expected stats results
+        _, tp_df, expected_stats = expected_transport_performance
+
+        # set arg under test to none, and mimic absence in expected_stats
+        default_args[arg_name] = None
+        expected_stats = expected_stats.drop(drop_expected_column, axis=1)
+
+        # calculate the stats, ensure arg set to none is not in results and
+        # all other results remain identical.
+        stats_df = _transport_performance_stats(tp_df, **default_args)
+        assert drop_expected_column not in stats_df.columns
+        assert_frame_equal(stats_df, expected_stats)
