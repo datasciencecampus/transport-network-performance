f3a61afb15a7866aa380803037d8b7891a72a4e1
Sergio Recio <sergio.recio.rodriguez@ons.gov.uk>
2023-11-30 15:26:44 +0000

    feat: added example notebook running analyse_network

---
+++ notebooks/analyse_network/analyse_network_timed.py
@@ -0,0 +1,39 @@
+"""Notebook to test single vs all origin runs, comparing execution time."""
+# %%
+import time
+
+from pyprojroot import here
+
+from transport_performance.analyse_network import AnalyseNetwork
+from transport_performance.utils.io import from_pickle
+
+# %%
+# inputs
+PATH_OSM = here("data/processed/osm/newport_latest_filtered.osm.pbf")
+PATH_GTFS = here("data/processed/gtfs/itm_newport_filtered_cleaned_gtfs.zip")
+PATH_RP = here("data/processed/population/newport_rp.pkl")
+
+newport_rp = from_pickle(PATH_RP)
+centroid_gdf = newport_rp.centroid_gdf
+
+# %%
+# initialise AnalyseNetwork object
+an = AnalyseNetwork(centroid_gdf, PATH_OSM, [PATH_GTFS])
+
+# %%
+# run od_matrix with all origins
+OUT_PATH_ALL = here("outputs/analyse_network/test_all")
+start = time.time()
+an.od_matrix(out_path=OUT_PATH_ALL, batch_orig=False)
+end = time.time()
+print(f"All origins time: {end - start}")
+
+# %%
+# run od_matrix with single origin
+OUT_PATH_ALL = here("outputs/analyse_network/test_single")
+start = time.time()
+an.od_matrix(out_path=OUT_PATH_ALL, batch_orig=True)
+end = time.time()
+print(f"All origins time: {end - start}")
+
+# %%
