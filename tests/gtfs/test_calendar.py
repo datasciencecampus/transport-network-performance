d4c721dcf0ab11109c662df3841fb8e6f312cdb0
r-leyshon <richard.leyshon@ons.gov.uk>
2024-01-18 16:20:13 +0000

    test: Calendar is as expected with no exception_type 2

--- tests/gtfs/test_calendar.py
+++ tests/gtfs/test_calendar.py
@@ -19,3 +19,38 @@ class TestCreateCalendarFromDates(object):
             create_calendar_from_dates(
                 calendar_dates=pd.DataFrame({"foo": 0}, index=[0])
             )
+
+    def test_create_calendar_passes_no_exception_type_2(self):
+        """Check output calendar is as epected without exception_type 2."""
+        mock_dates = pd.DataFrame(
+            {
+                "service_id": ["00A", "00A", "00A"],
+                "date": ["19840426", "19840426", "20240426"],
+                "exception_type": [1, 1, 1],
+            }
+        )
+        out_calendar = create_calendar_from_dates(mock_dates)
+        assert isinstance(
+            out_calendar, pd.DataFrame
+        ), f"Expected dataframe, found {type(out_calendar)}"
+        n_rows = len(out_calendar)
+        assert n_rows == 1, f"Expected df length 1, found {n_rows}"
+        # check values are as expected. Dates are Thu & Fri respectively.
+        pd.testing.assert_frame_equal(
+            out_calendar,
+            pd.DataFrame(
+                {
+                    "service_id": ["00A"],
+                    "monday": [0],
+                    "tuesday": [0],
+                    "wednesday": [0],
+                    "thursday": [1],
+                    "friday": [1],
+                    "saturday": [0],
+                    "sunday": [0],
+                    "start_date": ["19840426"],
+                    "end_date": ["20240426"],
+                },
+                index=[0],
+            ),
+        )
