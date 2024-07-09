6acdb31a34512f18d4441f8c870b212a23a73857
r-leyshon <richard.leyshon@ons.gov.uk>
2024-07-09 09:58:10 +0100

    chore: Rm src data

--- src/transport_performance/utils/constants.py
+++
@@ -1,6 +0,0 @@
-"""Constants to be used throughout the transport-performance package."""
-from importlib import resources as pkg_resources
-
-import transport_performance
-
-PKG_PATH = pkg_resources.files(transport_performance)
